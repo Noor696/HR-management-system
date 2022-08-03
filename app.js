14ae12544cf62f4db406461cb361da79aacce6ba
Noor696 <eng.nooralhateeb@gmail.com>
2022-08-03 08:36:01 +0300

    literal-object branch information

---
+++ app.js
@@ -0,0 +1,78 @@
+const employees =[
+     {
+    "Employee ID": this.valueID,
+    "Full Name" : "Ghazi Samer",
+    "Department" : "Administration" ,
+    "Level" :  "Senior",
+    "Image URL" : "",
+    "Salary" : ""
+  },
+
+   {
+    "Employee ID": this.valueID,
+    "Full Name" : "Lana Ali",
+    "Department" : "Finance" ,
+    "Level" :  "Senior",
+    "Image URL" : "",
+    "Salary" : ""
+  },
+
+   {
+    "Employee ID": this.valueID,
+    "Full Name" : "Tamara Ayoub",
+    "Department" : "Marketing" ,
+    "Level" :  "Senior",
+    "Image URL" : "",
+    "Salary" : ""
+  },
+
+   {
+    "Employee ID": this.valueID,
+    "Full Name" : "Safi Walid",
+    "Department" : "Administration" ,
+    "Level" :  "Mid-Senior",
+    "Image URL" : "",
+    "Salary" : ""
+  },
+   {
+    "Employee ID": this.valueID,
+    "Full Name" : "Omar Zaid",
+    "Department" : "Development" ,
+    "Level" :  "Senior",
+    "Image URL" : "",
+    "Salary" : ""
+  },
+ {
+    "Employee ID": this.valueID,
+    "Full Name" : "Rana Saleh",
+    "Department" : "Development" ,
+    "Level" :  "Junior",
+    "Image URL" : "",
+    "Salary" : ""
+  },
+   {
+    "Employee ID": this.valueID,
+    "Full Name" : "Hadi Ahmad",
+    "Department" : "Finance" ,
+    "Level" :  "Mid-Senior",
+    "Image URL" : "",
+    "Salary" : ""
+  },];
+
+
+  let valueID=5000;
+  for (let x =0; x < employees.length ; x++){
+    valueID+=1;
+}
+
+ //for (let x =0; x < employees.length ; x++){
+  //console.log(`Employee ID:${employees.valueID} `);
+  //console.log("Employee name:" + employees["Full Name"]+" ");
+  //console.log("Employee salary:"+employees.Salary + " ");}
+  //function getRandomArbitrary(min, max) {
+    //return Math.random() * (max - min) + min;}
+
+    console.log(employees);
+    
+  
+
